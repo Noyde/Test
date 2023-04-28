06e0365beea38c516015f3330af9d72cc4a9d5a6
Noyde <noyde@hotmail.com>
2023-04-28 21:26:01 +0200

    test

---
+++ Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.sidebar.success("Select a demo above.")
+
+st.write("# Welcome to Oval3 Analytics! 👋")
+
+st.markdown(
+    """
+    In construction
+"""
+)
--- Pages/1_Top10.py
+++ Pages/1_Top10.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
 st.set_page_config(page_title="Top 10 Sales")
+st.markdown("# Top 10 sales")
+st.sidebar.header("Top 10 sales")
